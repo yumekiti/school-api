d84425c1b0e4eb991f9f04813b6620731bfa9f73
yumekiti <yumekiti120440@gmail.com>
2024-02-01 03:15:57 +0900

    chore: add DateTime scalar

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,5 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 @Entity()
 @ObjectType()
 export class User {
@@ -12,7 +11,15 @@ export class User {
   @Field(() => Int, { description: 'School number' })
   schoolNumber: number;
 
-  @Column({ default: false }) // Set a default value for location_visibility if needed
+  @Column({ default: false })
   @Field(() => Boolean, { description: 'Location visibility' })
   location_visibility: boolean;
+
+  @CreateDateColumn()
+  @Field(() => Date, { description: 'Created at' })
+  created_at: Date;
+
+  @UpdateDateColumn()
+  @Field(() => Date, { description: 'Updated at' })
+  updated_at: Date;
 }
